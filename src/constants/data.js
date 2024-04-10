75076d6714a043327121d243b62566923d149d67
Sonikm <sonikm443@gmail.com>
2024-04-10 12:56:03 +0530

    fixes image issue

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,5 +1,5 @@
 
-export const DATA_IMG_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,c_fill/"
+export const DATA_IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
 // export const DATA_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940947&lng=85.1375645&page_type=DESKTOP_WEB_LISTING";
 export const DATA_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139391&lng=77.2090212&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
